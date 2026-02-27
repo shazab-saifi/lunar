1ed8cc3139e5f17bf18d3974034c6f2d40294527
shazab-saifi <shazebsaifi92@gmail.com>
2026-02-27 12:29:00 +0530

    Setup prettier, eslint, and husky

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,14 +1,32 @@
 import { defineConfig, globalIgnores } from "eslint/config";
 import nextVitals from "eslint-config-next/core-web-vitals";
 import nextTs from "eslint-config-next/typescript";
+import configPrettier from "eslint-config-prettier/flat";
 
 const eslintConfig = defineConfig([
   ...nextVitals,
   ...nextTs,
+  configPrettier,
+  {
+    files: ["**/*.ts", "**/*.tsx"],
+    rules: {
+      "@typescript-eslint/ban-ts-comment": "error",
+      "@typescript-eslint/no-explicit-any": "error",
+      "@typescript-eslint/no-unused-vars": [
+        "error",
+        { argsIgnorePattern: "^_", varsIgnorePattern: "^_" },
+      ],
+    },
+  },
   // Override default ignores of eslint-config-next.
   globalIgnores([
     // Default ignores of eslint-config-next:
     ".next/**",
+    ".husky/**",
+    ".vscode/**",
+    "node_modules/**",
+    "public/**",
+    "bun.lock",
     "out/**",
     "build/**",
     "next-env.d.ts",
